0edc771136c6050ec9b8ea41be9e784afafe1a59
Mikael Gibert <mgibert@meilleursagents.com>
2024-09-09 12:05:31 +0200

    chore(frontend): make app configurable

--- frontend/embro/src/index.js
+++ frontend/embro/src/index.js
@@ -2,16 +2,15 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import config from './auth_config.json';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <Auth0Provider
-    domain={config.domain}
-    clientId={config.clientId}
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     authorizationParams={{
-      audience:config.audience,
+      audience: process.env.REACT_APP_AUTH0_AUDIENCE,
       redirect_uri: window.location.origin
     }}
   >
