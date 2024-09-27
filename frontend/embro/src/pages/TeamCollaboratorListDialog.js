a35bffad0b9c1d09ffbcc64bc5670038ed9de583
Mikael Gibert <mgibert@meilleursagents.com>
2024-09-27 17:42:07 +0200

    feat(membership): make collaborators searchable

--- frontend/embro/src/pages/TeamCollaboratorListDialog.js
+++ frontend/embro/src/pages/TeamCollaboratorListDialog.js
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Box, Dialog, List, ListItem, ListItemText } from "@mui/material";
+import { Box, Dialog, DialogTitle, List, ListItem, ListItemText, TextField } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { OrganizationContext } from "../context/OrganizationContext";
 import { listCollaborators } from "../utils/api";
@@ -9,11 +9,27 @@ function TeamCollaboratorListDialog({open, team, members, onSelect, onClose}) {
   const { getAccessTokenSilently } = useAuth0();
   const { currentOrganization} = useContext(OrganizationContext);
   const [visible, setVisible] = useState(open);
+  const [filteredCollaborators, setFilteredCollaborators] = useState([]);
+  const [search, setSearch] = useState('');
 
   const handleSelection = (collaborator) => {
     onSelect(collaborator);
     onClose();
   };
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    setFilteredCollaborators(filterCollaborators(e.target.value));
+  };
+
+  const filterCollaborators = (firstChars) => {
+    return (
+      collaborators.filter((c) => (
+        c.firstname.toLowerCase().startsWith(firstChars.toLowerCase()) ||
+        c.lastname.toLowerCase().startsWith(firstChars.toLowerCase()))
+      ).filter(c => !members.find(m => m.collaboratorId === c.id))
+    );
+  }
 
   useEffect(() => {
     setVisible(open);
@@ -26,19 +42,26 @@ function TeamCollaboratorListDialog({open, team, members, onSelect, onClose}) {
       }
       const token = await getAccessTokenSilently();
       const loaded = await listCollaborators(token, currentOrganization?.id);
-      setCollaborators(loaded.filter((c) => !members.find(m => m.collaboratorId === c.id)));
+      setFilteredCollaborators(loaded.filter(c => !members.find(m => m.collaboratorId === c.id)));
+      setCollaborators(loaded);
+      console.log(loaded);
     }
     load();
   }, [team, members, getAccessTokenSilently, currentOrganization]);
 
   return (
-    <Dialog
-      open={visible} 
-      onClose={onClose}
-    >
+    <Dialog open={visible} onClose={onClose}>
+      <DialogTitle>Pick a collaborator to add to {team?.name}</DialogTitle>
+      <TextField
+        label="Search"
+        variant="outlined"
+        value={search}
+        onChange={handleSearchChange}
+        sx={{ m: 2 }}
+      />
       <Box>
-        <List dense={true}>
-        {collaborators.map((c) => (
+        <List>
+        {filteredCollaborators.map((c) => (
           <ListItem key={c.id} button onClick={() => handleSelection(c)}>
             <ListItemText primary={`${c.firstname} ${c.lastname}`} secondary={c.email} />
           </ListItem>
